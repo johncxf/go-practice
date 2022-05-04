b5fee9f6a0099059c6566494046647b1388762b7
chenxinfang <13777719513@163.com>
2022-05-04 23:12:49 +0800

    update

--- algorithm/singleLinkedList.go
+++ algorithm/singleLinkedList.go
@@ -22,10 +22,10 @@ func addNode(t *Node, v int) int {
 		return 0
 	}
 
-	if v == t.Value {
-		fmt.Println("节点已存在:", v)
-		return -1
-	}
+	// if v == t.Value {
+	// 	fmt.Println("节点已存在:", v)
+	// 	return -1
+	// }
 
 	// 如果当前节点下一个节点为空
 	if t.Next == nil {
@@ -83,7 +83,7 @@ func size(t *Node) int {
 /**
  * 从尾到头打印链表
  *
- * @param head ListNode类
+ * @param head Node类
  * @return int整型一维数组
  */
 func printListFromTailToHead(head *Node) []int {
@@ -107,13 +107,13 @@ func printListFromTailToHead(head *Node) []int {
 }
 
 /**
- * 链表中倒数最后k个结点
+ * 链表中倒数最后 k 个结点
  *
- * @param pHead ListNode类
+ * @param pHead Node类
  * @param k int整型
- * @return ListNode类
+ * @return Node类
  */
-func FindKthToTail(pHead *Node, k int) *Node {
+func findKthToTail(pHead *Node, k int) *Node {
 	// 边界值
 	if pHead == nil || k <= 0 {
 		return nil
@@ -138,24 +138,132 @@ func FindKthToTail(pHead *Node, k int) *Node {
 	return prev
 }
 
+/**
+ * [JZ18-简单]删除链表的节点
+ *
+ * @param head Node类
+ * @param val int整型
+ * @return Node类
+ */
+func deleteNode(head *Node, val int) *Node {
+	if head.Value == val {
+		return head.Next
+	}
+	pre := head
+	for nil != head {
+		if head.Value == val {
+			head.Value = head.Next.Value
+			head.Next = head.Next.Next
+			break
+		}
+		head = head.Next
+	}
+	// 简化 - 直接从 head 的 Next 节点开始遍历
+	// for head.Next.Value != val {
+	// 	head = head.Next
+	// }
+	// head.Next = head.Next.Next
+	return pre
+}
+
+/**
+ * [JZ24-简单]反转链表
+ *
+ * @param pHead Node类
+ * @return Node类
+ */
+func reverseList(head *Node) *Node {
+	// tmp := make([]int, 0)
+	// for nil != head {
+	// 	tmp = append(tmp, head.Value)
+	// 	head = head.Next
+	// }
+
+	// head = new(Node)
+	// tmpLen := len(tmp) - 1
+	// for tmpLen >= 0 {
+	// 	addNode(head, tmp[tmpLen])
+	// 	tmpLen--
+	// }
+
+	var newHead *Node
+	var tmp *Node
+	for head != nil {
+		// 暂存下一个节点
+		tmp = head.Next
+		// 把新链表挂到原链表后面
+		head.Next = newHead
+		// 更新新链表
+		newHead = head
+		// 移动指针
+		head = tmp
+	}
+
+	return newHead
+}
+
+/**
+ * [JZ76-简单]删除链表中重复的结点
+ *
+ * @param pHead Node类
+ * @return Node类
+ */
+func deleteDuplication(pHead *Node) *Node {
+	// 定义一个空链表
+	var res = new(Node)
+	// 给新链表添加表头
+	res.Next = pHead
+	cur := res
+	// 从 next 节点开始遍历（前面加了一个表头）
+	for nil != cur.Next && nil != cur.Next.Next {
+		if cur.Next.Value == cur.Next.Next.Value {
+			// 遇到前后节点相同情况，进行遍历，跳过所有相同节点
+			tmp := cur.Next.Value
+			for nil != cur.Next && cur.Next.Value == tmp {
+				cur.Next = cur.Next.Next
+			}
+		} else {
+			// 循环链表
+			cur = cur.Next
+		}
+	}
+	return res.Next
+}
+
 func main() {
 	fmt.Println(head)
 	head = nil
 	// 遍历链表
 	traverse(head)
-	// 添加节点
+	// // 添加节点
+	// addNode(head, 1)
+	// addNode(head, -1)
+	// // 再次遍历
+	// traverse(head)
+	// // 添加更多节点
+	// addNode(head, 10)
+	// addNode(head, 5)
+	// addNode(head, 45)
+	// // 添加已存在节点
+	// addNode(head, 5)
+	// // 再次遍历
+	// traverse(head)
+
 	addNode(head, 1)
-	addNode(head, -1)
-	// 再次遍历
-	traverse(head)
-	// 添加更多节点
-	addNode(head, 10)
+	addNode(head, 2)
+	addNode(head, 2)
+	addNode(head, 3)
+	addNode(head, 3)
 	addNode(head, 5)
-	addNode(head, 45)
-	// 添加已存在节点
-	addNode(head, 5)
-	// 再次遍历
 	traverse(head)
+
+	// deleteNode(head, 5)
+
+	// ret := reverseList(head)
+	// traverse(ret)
+
+	ret := deleteDuplication(head)
+	traverse(ret)
 
 	// res := printListFromTailToHead(head)
 	// fmt.Println(res)
