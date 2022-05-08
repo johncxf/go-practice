2c8f97676e1013f8bf250b2af01ae0c68e927826
chenxinfang <13777719513@163.com>
2022-05-08 23:44:42 +0800

    update

--- algorithm/singleLinkedList.go
+++ algorithm/singleLinkedList.go
@@ -230,6 +230,32 @@ func deleteDuplication(pHead *Node) *Node {
 	return res.Next
 }
 
+/**
+ * [JZ23-中等]链表中环的入口结点
+ *
+ * @param pHead Node类
+ * @return Node类
+ */
+func entryNodeOfLoop(pHead *Node) *Node {
+	if nil == pHead || nil == pHead.Next {
+		return nil
+	}
+	fast, slow := pHead, pHead
+	for nil != fast && nil != fast.Next.Next {
+		fast = fast.Next.Next
+		slow = slow.Next
+		if fast == slow {
+			fast = pHead
+			for fast != slow {
+				fast = fast.Next
+				slow = slow.Next
+			}
+			return fast
+		}
+	}
+	return nil
+}
+
 func main() {
 	fmt.Println(head)
 	head = nil
