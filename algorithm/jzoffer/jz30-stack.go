02ea872c544490a6463dbf8c88c10abf6530ebad
john <13777719513@163.com>
2022-06-12 23:18:03 +0800

    update

---
+++ algorithm/jzoffer/jz30-stack.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+)
+
+var stack []int
+
+func push(node int) {
+    stack = append(stack, node)
+}
+
+func pop() {
+    stack = stack[0:len(stack)-1]
+}
+
+func top() int {
+    result := stack[len(stack) - 1]
+    return result
+}
+
+func min() int {
+    min := stack[0]
+    for _, v := range stack {
+        if min > v {
+            min = v
+        }
+    }
+    return min
+}
+
+func main() {
+    push(-1)
+    push(2)
+
+    a := min()
+    b := top()
+    fmt.Println(a)
+    fmt.Println(b)
+}
