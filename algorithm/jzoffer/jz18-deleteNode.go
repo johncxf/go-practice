7a645ad17c51609e28f030f0838cb10a64ff1ee9
john_chen <chenxinfang.john@gmail.com>
2023-08-29 00:10:49 +0800

    update

---
+++ algorithm/jzoffer/jz18-deleteNode.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	. "go_practice/pkg/datastruct"
+)
+
+/**
+* [JZ18-简单] 删除链表的节点
+*
+* @param head ListNode类
+* @param val int整型
+* @return ListNode类
+ */
+func deleteNode(head *ListNode, val int) *ListNode {
+	if head.Val == val {
+		return head.Next
+	}
+	pre := head
+	for nil != head {
+		if head.Val == val {
+			head.Val = head.Next.Val
+			head.Next = head.Next.Next
+			break
+		}
+		head = head.Next
+	}
+	return pre
+}
+
+func main() {
+	fmt.Println("")
+	var head1 *ListNode
+
+	head1 = AddNode2(head1, 1)
+	head1 = AddNode2(head1, 2)
+	head1 = AddNode2(head1, 3)
+	head1 = AddNode2(head1, 6)
+	head1 = AddNode2(head1, 7)
+
+	rHead := deleteNode(head1, 3)
+	TraverseSingleList(rHead)
+}
---
+++ algorithm/jzoffer/jz52-findFirstCommonNode.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"fmt"
+	. "go_practice/pkg/datastruct"
+	"reflect"
+)
+
+/**
+ * [JZ52-简单] 两个链表的第一个公共结点
+ *
+ * @param pHead1 ListNode类
+ * @param pHead2 ListNode类
+ * @return ListNode类
+ */
+func findFirstCommonNode(pHead1 *ListNode, pHead2 *ListNode) *ListNode {
+	p1, p2 := pHead1, pHead2
+	for reflect.DeepEqual(p1, p2) == false {
+		if p1 == p2 {
+			fmt.Println(p1.Val)
+		}
+		if p1 != nil {
+			p1 = p1.Next
+		} else {
+			p1 = pHead2
+		}
+
+		if p2 != nil {
+			p2 = p2.Next
+		} else {
+			p2 = pHead1
+		}
+	}
+	return p1
+}
+
+func main() {
+	fmt.Println("")
+	var head1 *ListNode
+	var head2 *ListNode
+
+	head1 = AddNode2(head1, 1)
+	head1 = AddNode2(head1, 2)
+	head1 = AddNode2(head1, 3)
+	head1 = AddNode2(head1, 6)
+	head1 = AddNode2(head1, 7)
+
+	head2 = AddNode2(head2, 4)
+	head2 = AddNode2(head2, 5)
+	head2 = AddNode2(head2, 6)
+	head2 = AddNode2(head2, 7)
+
+	rHead := findFirstCommonNode(head1, head2)
+	TraverseSingleList(rHead)
+}
