3e8a0b8da4726ed8528840c4f63e7b4eb97a62d3
john_chen <chenxinfang.john@gmail.com>
2024-03-30 21:39:57 +0800

    update

--- algorithm/leetcode/lt121-maxProfit.go
+++ algorithm/leetcode/lt121-maxProfit.go
@@ -1,1 +1,22 @@
 package main
+
+import "fmt"
+
+func maxProfit(prices []int) int {
+	minPro := int(1e5)
+	maxPro := 0
+	for i := 0; i < len(prices); i++ {
+		if prices[i] < minPro {
+			minPro = prices[i]
+		}
+		if prices[i]-minPro > maxPro {
+			maxPro = prices[i] - minPro
+		}
+	}
+	return maxPro
+}
+
+func main() {
+	fmt.Println(maxProfit([]int{7, 1, 5, 3, 6, 4}))
+	fmt.Println(maxProfit([]int{7, 6, 4, 3, 1}))
+}
--- algorithm/leetcode/lt70-climbStairs.go
+++ algorithm/leetcode/lt70-climbStairs.go
@@ -1,1 +1,22 @@
 package main
+
+import "fmt"
+
+func climbStairs(n int) int {
+	dp := make([]int, n)
+	if n <= 2 {
+		return n
+	}
+	dp[0] = 1
+	dp[1] = 2
+	for i := 2; i < n; i++ {
+		dp[i] = dp[i-1] + dp[i-2]
+	}
+	return dp[n-1]
+}
+
+func main() {
+	fmt.Println(climbStairs(1))
+	fmt.Println(climbStairs(2))
+	fmt.Println(climbStairs(3))
+}
