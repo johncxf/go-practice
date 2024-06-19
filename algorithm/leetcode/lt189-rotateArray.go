a6bde48fcc1bec1f45e5314f7387015e8ffe3d11
john_chen <chenxinfang.john@gmail.com>
2024-06-20 00:36:08 +0800

    update

---
+++ algorithm/leetcode/lt189-rotateArray.go
@@ -0,0 +1,18 @@
+package main
+
+// [L189-中等] 轮转数组
+func rotateArray(nums []int, k int) {
+	n := len(nums)
+	// 处理 k > n 的情况
+	k = k % n
+	arr1 := nums[:n-k]
+	arr2 := nums[n-k:]
+	ans := append(arr2, arr1...)
+	copy(nums, ans)
+}
+
+func main() {
+	rotateArray([]int{1, 2, 3, 4, 5, 6, 7}, 3)
+	rotateArray([]int{-1, -100, 3, 99}, 2)
+	rotateArray([]int{-1}, 2)
+}
---
+++ algorithm/leetcode/lt238-productExceptSelf.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func productExceptSelf(nums []int) []int {
+	n := len(nums)
+	// 初始化两个数组，分别存储 i 左右两侧的乘机
+	lArr, rArr := make([]int, n), make([]int, n)
+	lArr[0] = 1
+	for i := 1; i < n; i++ {
+		lArr[i] = lArr[i-1] * nums[i-1]
+	}
+	rArr[n-1] = 1
+	for i := n - 2; i >= 0; i-- {
+		rArr[i] = rArr[i+1] * nums[i+1]
+	}
+	// i 的值为左右两侧乘机
+	ans := make([]int, n)
+	for i := 0; i < n; i++ {
+		ans[i] = lArr[i] * rArr[i]
+	}
+	return ans
+}
+
+func main() {
+	fmt.Println(productExceptSelf([]int{1, 2, 3, 4}))
+}
