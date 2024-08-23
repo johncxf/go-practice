a34423f385c2ad54711ca7ff4616360489f39d10
john_chen <chenxinfang.john@gmail.com>
2024-08-23 14:33:52 +0800

    update

--- algorithm/leetcode/hot100/skill/lt287-findDuplicate.go
+++ algorithm/leetcode/hot100/skill/lt287-findDuplicate.go
@@ -3,6 +3,7 @@ package main
 
 import "fmt"
 
+// 快慢指针
 func findDuplicate(nums []int) int {
 	// 定义快慢指针
 	slow, fast := 0, 0
@@ -21,6 +22,7 @@ func findDuplicate(nums []int) int {
 	return cur
 }
 
+// 辅助数组
 func findDuplicate2(nums []int) int {
 	tmp := make([]int, len(nums))
 	for _, num := range nums {
@@ -32,9 +34,31 @@ func findDuplicate2(nums []int) int {
 	return -1
 }
 
+// 利用原数组作为辅助数组
+func findDuplicate3(nums []int) int {
+	// 遍历所有元素
+	for _, num := range nums {
+		// 如果该位置为负数，说明被修改过，也就是存在重复
+		if nums[abs(num)] < 0 {
+			return abs(num)
+		}
+		// 修改以当前元素绝对值为下标位置的值为负数
+		nums[abs(num)] = -nums[abs(num)]
+	}
+	return -1
+}
+
+func abs(num int) int {
+	if num < 0 {
+		return -num
+	}
+	return num
+}
+
 func main() {
 	fmt.Println(findDuplicate([]int{1, 3, 4, 2, 2}))
 
 	fmt.Println(findDuplicate2([]int{1, 3, 4, 2, 2}))
 
+	fmt.Println(findDuplicate3([]int{1, 3, 4, 2, 2}))
 }
